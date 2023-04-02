e77372f9ac74570cfc2c23a7446af7529030db46
oozguroo <ozgurraykut@gmail.com>
2023-04-02 19:03:33 +0300

    en of section 19

--- client/src/app/_services/message.service.ts
+++ client/src/app/_services/message.service.ts
@@ -7,6 +7,7 @@ import { Group } from '../_models/group';
 import { Message } from '../_models/message';
 import { User } from '../_models/user';
 import { getPaginatedResult, getPaginationHeaders } from './paginationHelper';
+import { BusyService } from './busy.service';
 
 @Injectable({
   providedIn: 'root',
@@ -18,19 +19,24 @@ export class MessageService {
   private messageThreadSource = new BehaviorSubject<Message[]>([]);
   messageThread$ = this.messageThreadSource.asObservable();
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient,
+    private busyService:BusyService
+    ) {}
 
   createHubConnection(user: User, otherUsername: string){
+    this.busyService.busy();
     this.hubConnection = new HubConnectionBuilder()
     .withUrl(this.hubUrl + 'message?user=' + otherUsername,{
       accessTokenFactory:  () => user.token
     })
     .withAutomaticReconnect()
     .build();
-    this.hubConnection.start().catch(error => console.log(error));
+    this.hubConnection.start().catch(error => console.log(error))
+    .finally(() => this.busyService.idle());
 
     this.hubConnection.on('ReceiveMessageThread', messages => {
         this.messageThreadSource.next(messages);
+        
     })
 
     this.hubConnection.on('UpdatedGroup',(group:Group) => {
@@ -59,6 +65,7 @@ export class MessageService {
 
   stopHubConnection(){
   if(this.hubConnection){
+    this.messageThreadSource.next([]);
     this.hubConnection.stop();
   }
   }
--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -14,6 +14,7 @@ export class MemberMessagesComponent implements OnInit {
   @Input() username?: string;
 
   messageContent = '';
+  loading = false;
 
   constructor(public messageService: MessageService) {}
 
@@ -21,10 +22,11 @@ export class MemberMessagesComponent implements OnInit {
 
   sendMessage() {
     if (!this.username) return;
+    this.loading=true;
     this.messageService
       .sendMessage(this.username, this.messageContent)
       .then(() => {
         this.messageForm?.reset();
-      });
+      }).finally(() => this.loading = false);
   }
 }
